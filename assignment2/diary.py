dfdc454832fa8e4a902f35892dd3833742745e60
risquaMussa <rismussa@gmail.com>
2026-05-05 20:16:46 -0400

    revision

--- assignment2/diary.py
+++ assignment2/diary.py
@@ -19,7 +19,9 @@ except Exception as e:
    trace_back = traceback.extract_tb(e.__traceback__)
    stack_trace = []
    for trace in trace_back:
-      stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+      stack_trace.append(
+          f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+   
    print(f"Exception type: {type(e).__name__}")
    message = str(e)
    if message:
