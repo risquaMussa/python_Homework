2a6043540aeef2974a1666c7eecfd1646eadf6f3
risquaMussa <rismussa@gmail.com>
2026-05-05 20:38:15 -0400

    revision

--- assignment2/diary.py
+++ assignment2/diary.py
@@ -11,19 +11,25 @@ try:
                 first = False
             else:
                 entry = input("What else? ")
-                file.write(entry + "\n")
+            
+            file.write(entry + "\n")
            
             if entry == "done for now":
                 break 
 except Exception as e:
-   trace_back = traceback.extract_tb(e.__traceback__)
-   stack_trace = []
-   for trace in trace_back:
-      stack_trace.append(
-          f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+    print("An exception occurred.")
+    
+    trace_back = traceback.extract_tb(e.__traceback__)
+    stack_trace = []
    
-   print(f"Exception type: {type(e).__name__}")
-   message = str(e)
-   if message:
-      print(f"Exception message: {message}")
-   print(f"Stack trace: {stack_trace}")  
+    for trace in trace_back:
+        stack_trace.append(
+            f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+   
+    print(f"Exception type: {type(e).__name__}")
+    message = str(e)
+   
+    if message:
+       print(f"Exception message: {message}")
+
+    print(f"Stack trace: {stack_trace}")  
